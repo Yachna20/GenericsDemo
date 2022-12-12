f20523e2273cf13420e63514cdf92e0c237f0bcf
Yachna20 <thakreyachna20@gmail.com>
2022-12-12 22:37:45 +0530

    [Yachna]-Given float is maximum at third position

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -4,7 +4,7 @@
     {
     static void Main(string[] args)
       {
-        float max = FindMax.FindFloatMax(3.3f, 8.7f, 4.4f);
+        float max = FindMax.FindFloatMax(3.3f, 4.4f, 8.7f);
         Console.WriteLine(max);
       }
     }
