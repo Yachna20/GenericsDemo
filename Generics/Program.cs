7a468392e0c275550efe8e65cb87c42522f73836
Yachna20 <thakreyachna20@gmail.com>
2022-12-12 22:32:35 +0530

    [Yachna]-Given float is maximum at second position

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -4,7 +4,7 @@
     {
     static void Main(string[] args)
       {
-        float max = FindMax.FindFloatMax(8.7f, 3.3f, 4.4f);
+        float max = FindMax.FindFloatMax(3.3f, 8.7f, 4.4f);
         Console.WriteLine(max);
       }
     }
