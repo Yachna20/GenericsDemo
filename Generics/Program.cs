4a51a1dece348ac2f9a45b2f6a09f59b57425f3f
Yachna20 <thakreyachna20@gmail.com>
2022-12-12 17:21:35 +0530

    [Yachna]- Given maximum number at first position

---
+++ Generics/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Generics
+{
+    public class Program
+    {
+    static void Main(string[] args)
+      {
+        int max = FindMax.FindIntMax(8, 3, 4);
+        Console.WriteLine(max);
+
+
+      }
+    }
+}
+
+
