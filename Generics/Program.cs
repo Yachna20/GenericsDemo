6bcd093030141448279b2fa73716a7b4ab76b55e
Yachna20 <thakreyachna20@gmail.com>
2022-12-17 23:40:11 +0530

    [Yachna]-Refactor to create generic class

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -4,11 +4,11 @@
     {
     static void Main(string[] args)
       {
-            int max= GenericsMax.FindMaxNumber<int>(44, 22, 12);
+            int max= Generics_Max<int>.FindMaxNo(44, 22, 12);
             Console.WriteLine(max);
-            float floatmax = GenericsMax.FindMaxNumber<float>(4.4f, 2.2f, 1.1f);
+            float floatmax = Generics_Max<float>.FindMaxNo(4.4f, 2.2f, 1.1f);
             Console.WriteLine(floatmax);
-            string stringmax = GenericsMax.FindMaxNumber<string>("Pear", "Apple", "Banana");
+            string stringmax = Generics_Max<string>.FindMaxNo("Pear", "Apple", "Banana");
             Console.WriteLine(stringmax);
         }
     }
