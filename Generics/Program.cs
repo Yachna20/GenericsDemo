ed146f79816140769e6474d2db1cda33a4893f83
Yachna20 <thakreyachna20@gmail.com>
2022-12-17 23:09:54 +0530

    [Yachna]-Refactor all the 3 to 1 generic method to find maximum

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -4,9 +4,13 @@
     {
     static void Main(string[] args)
       {
-            string max = FindMax.FindStringMax("Banana", "Apple", "Pear");
+            int max= GenericsMax.FindMaxNumber<int>(44, 22, 12);
             Console.WriteLine(max);
-      }
+            float floatmax = GenericsMax.FindMaxNumber<float>(4.4f, 2.2f, 1.1f);
+            Console.WriteLine(floatmax);
+            string stringmax = GenericsMax.FindMaxNumber<string>("Pear", "Apple", "Banana");
+            Console.WriteLine(stringmax);
+        }
     }
 }
 
